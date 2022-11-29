b87257a09ab3829713841014773657a674c6ad47
Alexey Miheev <miheev_aa@groupbwt.com>
2022-11-29 18:42:10 +0200

    feat(tests): update code style in tests

--- tests/Unit/Util/DateTimeUtilTest.php
+++ tests/Unit/Util/DateTimeUtilTest.php
@@ -10,7 +10,7 @@ use PHPUnit\Framework\TestCase;
 class DateTimeUtilTest extends TestCase
 {
     /**
-     * @covers \App\CommissionTask\Util\DateTimeUtil::getWeekIdentifier
+     * @covers       \App\CommissionTask\Util\DateTimeUtil::getWeekIdentifier
      * @dataProvider dataProviderForGetWeekIdentifierTesting
      */
     public function testGetWeekIdentifier(\DateTimeInterface $date, string $expectedResult): void
@@ -24,22 +24,10 @@ class DateTimeUtilTest extends TestCase
     public function dataProviderForGetWeekIdentifierTesting(): array
     {
         return [
-            [
-                new \DateTime('2022-11-29'),
-                '2022-11-28-2022-12-04'
-            ],
-            [
-                new \DateTime('2022-04-14'),
-                '2022-04-11-2022-04-17'
-            ],
-            [
-                new \DateTime('2020-01-01'),
-                '2019-12-30-2020-01-05'
-            ],
-            [
-                new \DateTime('2023-01-11'),
-                '2023-01-09-2023-01-15'
-            ]
+            [new \DateTime('2022-11-29'), '2022-11-28-2022-12-04'],
+            [new \DateTime('2022-04-14'), '2022-04-11-2022-04-17'],
+            [new \DateTime('2020-01-01'), '2019-12-30-2020-01-05'],
+            [new \DateTime('2023-01-11'), '2023-01-09-2023-01-15']
         ];
     }
 }
