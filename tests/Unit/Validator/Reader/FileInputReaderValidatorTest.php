cbf6318fc790cdc71a1f5148121697d3ffcb6dc7
Alexey Miheev <miheev_aa@groupbwt.com>
2023-11-02 18:20:10 +0200

    feature(phpunit): cover Validator dir with phpunit unit tests

---
+++ tests/Unit/Validator/Reader/FileInputReaderValidatorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\CommissionTask\Tests\Unit\Validator\Reader;
+
+use App\CommissionTask\Exception\Validator\Reader\UnsupportedClientTypeException;
+use App\CommissionTask\Exception\Validator\Reader\UnsupportedOperationTypeException;
+use App\CommissionTask\Validator\Reader\FileInputReaderValidator;
+use PHPUnit\Framework\TestCase;
+
+class FileInputReaderValidatorTest extends TestCase
+{
+    public function testValidateThrowsUnsupportedClientTypeException(): void
+    {
+        $validator = new FileInputReaderValidator(['private'], []);
+        $this->expectException(UnsupportedClientTypeException::class);
+        $this->expectExceptionMessage('Got unsupported client type: business. Supported in config: private');
+        $validator->validate(['client_type' => 'business']);
+    }
+
+    public function testValidateThrowsUnsupportedOperationTypeException(): void
+    {
+        $validator = new FileInputReaderValidator(['private'], ['deposit']);
+        $this->expectException(UnsupportedOperationTypeException::class);
+        $this->expectExceptionMessage('Got unsupported operation type: withdraw. Supported in config: deposit');
+        $validator->validate(['client_type' => 'private', 'operation_type' => 'withdraw']);
+    }
+
+    public function testValidate(): void
+    {
+        $validator = new FileInputReaderValidator(['private'], ['deposit']);
+        $this->expectNotToPerformAssertions();
+        $validator->validate(['client_type' => 'private', 'operation_type' => 'deposit']);
+    }
+}
